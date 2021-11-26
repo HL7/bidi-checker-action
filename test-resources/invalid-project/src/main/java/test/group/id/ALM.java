ed7017da61a840e4d86d082080e10a906428b688
dotasek <dotasek.dev@gmail.com>
2021-11-26 10:31:14 -0500

    Fix misnamed file

---
+++ test-resources/invalid-project/src/main/java/test/group/id/ALM.java
@@ -0,0 +1,13 @@
+package test.group.id;
+
+/**
+ * ؜
+ * Bidi characters above
+ */
+public class ALM
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
